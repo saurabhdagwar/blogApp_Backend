e10092a7d1d15783a212fd0ba51a094d6b6a5934
Neosoft <saurabh.dagwar@neosoftmail.com>
2024-05-31 17:06:31 +0530

    updated the vercel accounts details

--- app.js
+++ app.js
@@ -3,19 +3,32 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
+// import serverless from "serverless-http";
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
+
 mongoose
   .connect(process.env.MONGO_URL)
-  //   .connect(
-  //     "mongodb+srv://saurabhdagwar2020:DC5yPk2MIDjan8k3@cluster0.ucnsvve.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0"
-  //   )
-  .then(() => app.listen(PORT))
-  .then(() =>
-    console.log(`connected to the database and Listning to port ${PORT}`)
-  )
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
+
+// If running locally, start the Express server
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+// Export the app wrapped in serverless-http for AWS Lambda
+// export const handler = serverless(app);
